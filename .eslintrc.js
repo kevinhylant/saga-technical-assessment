b9830cbc2d87357c5c8a01c74b8da27a53ab8bd4
Kevin Hylant <kevin.hylant@gmail.com>
2020-05-13 14:42:05 -0400

    Add and fetch local json data files

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     "react/prop-types": "off",
     "react/display-name": "off",
+    "@typescript-eslint/no-var-requires": "off",
   }
 };
