2dab8e3d1f43db3811e88aa6fcef7fa7a5e95953
Kevin Hylant <kevin.hylant@gmail.com>
2020-05-18 18:10:01 -0400

    Layer in more sophisticated, less hacky logic for when to show the time slider value while seeking/sliding

--- src/features/storyPlayer/StoryPlayerProgressBar.tsx
+++ src/features/storyPlayer/StoryPlayerProgressBar.tsx
@@ -1,9 +1,6 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
-import TrackPlayer, {
-  usePlaybackState,
-  useTrackPlayerProgress,
-} from 'react-native-track-player';
+import TrackPlayer, { useTrackPlayerProgress } from 'react-native-track-player';
 import Slider from '@react-native-community/slider';
 
 import { Message } from '../../components/Message';
@@ -16,39 +13,28 @@ interface Props {
   lengthMs: number;
 }
 
-function usePrevious(value) {
-  // The ref object is a generic container whose current property is mutable ...
-  // ... and can hold any value, similar to an instance property on a class
-  const ref = useRef();
-  
-  // Store current value in ref
-  useEffect(() => {
-    ref.current = value;
-  }, [value]); // Only re-run if value changes
-  
-  // Return previous value (happens before update in useEffect above)
-  return ref.current;
-}
-
 export const StoryPlayerProgressBar: React.FunctionComponent<Props> = ({
   lengthMs, // we pass in the duration rather than grabbing it from useTrackPlayerProgress because while the player is loading before first play, the duration is 0
 }) => {
   const { position } = useTrackPlayerProgress();
-  const previousPosition = usePrevious(position) ?? 0;
-  const playbackState = usePlaybackState();
   const [isSliding, setIsSliding] = useState(false);
+  const [shouldUseSliderPosition, setShouldUseSliderPosition] = useState(false);
   const [slidePositionMs, setSlidePositionMs] = useState(position);
 
   useEffect(() => {
-    // when the position controlled by the react-native-track-player changes
-    // AND the user is not actively adjusting the slider, we want to unset
-    // the override and show the actual progress position
-    console.log({ previousPosition, position });
-    const positionChange = Math.abs(position - previousPosition);
-    const isChangeResultOfSlide = positionChange > 3;
-    // if (props.position)
-    if (isChangeResultOfSlide) {
-      setIsSliding(false); // By un-setting, we fall back to react-native-track-player's position
+    // IF we are currently showing the slider’s position
+    // AND the user is not actively sliding
+    // AND the position managed by react-native-track-player changes (e.g. the track is playing)
+    // THEN we want to show the track's real position rather instead of the slider’s
+    //
+    // - In the case that the audio track is paused, the 'position' does not get updated immediately
+    // by react-native-track-player’s state hook immediately (not until the user taps play) and so
+    // we want to continue showing the slider’s value until the 'position' state updates to reflect
+    // the value set by TrackPlayer.seekTo(). This effect block only gets called when the position changes
+    // which handles this scenario.
+    if (shouldUseSliderPosition && !isSliding) {
+      // setIsSliding(false); // By un-setting, we fall back to react-native-track-player's position
+      setShouldUseSliderPosition(false);
     }
   }, [position]);
 
@@ -58,22 +44,25 @@ export const StoryPlayerProgressBar: React.FunctionComponent<Props> = ({
   // if it is sliding while playing the clip, show the slide position
   // if it is sliding while paused, show the slide position
   // when done sliding and paused, continue showing slide position until there is a position set
-  const positionToDisplay = isSliding ? slidePositionMs : positionMs;
+  const positionToDisplay = shouldUseSliderPosition
+    ? slidePositionMs
+    : positionMs;
   const durationIntoStory = msToMinutesAndSeconds(positionToDisplay);
   const durationUntilEnd = msToMinutesAndSeconds(lengthMs - positionToDisplay);
 
-  function onSlidingStart() {
+  function onSlidingStart(): void {
     setIsSliding(true);
+    setShouldUseSliderPosition(true);
   }
 
-  function onValueChange(msValue) {
+  function onValueChange(msValue: number): void {
     setSlidePositionMs(msValue);
   }
 
-  function onSlidingComplete(msValue) {
+  function onSlidingComplete(msValue: number): void {
     const seconds = msToSeconds(msValue);
     TrackPlayer.seekTo(seconds);
-    // setIsSliding(false);
+    setIsSliding(false);
   }
 
   return (
